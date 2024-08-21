3a18b5f3303e596569fda115d38cdd9acd1c0df9
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-08-21 15:27:29 +0200

    wip

--- co-noir/ultrahonk/src/oink/prover.rs
+++ co-noir/ultrahonk/src/oink/prover.rs
@@ -1,8 +1,9 @@
 use crate::{
     transcript::Keccak256Transcript,
-    types::{ProverCrs, ProvingKey},
+    types::{ProverCrs, ProverMemory, ProvingKey},
 };
 use ark_ec::{pairing::Pairing, VariableBaseMSM};
+use ark_ff::{One, Zero};
 use std::{io, marker::PhantomData};
 
 type HonkProofResult<T> = std::result::Result<T, HonkProofError>;
@@ -21,12 +22,14 @@ pub enum HonkProofError {
 }
 
 pub struct Plonk<P: Pairing> {
+    memory: ProverMemory<P>,
     phantom_data: PhantomData<P>,
 }
 
 impl<P: Pairing> Plonk<P> {
     pub fn new() -> Self {
         Self {
+            memory: ProverMemory::default(),
             phantom_data: PhantomData,
         }
     }
@@ -62,6 +65,7 @@ impl<P: Pairing> Plonk<P> {
 
     // Compute first three wire commitments
     fn execute_wire_commitments_round(
+        &mut self,
         transcript: &mut Keccak256Transcript<P>,
         proving_key: &ProvingKey<P>,
     ) -> HonkProofResult<()> {
@@ -69,27 +73,95 @@ impl<P: Pairing> Plonk<P> {
 
         // Commit to the first three wire polynomials of the instance
         // We only commit to the fourth wire polynomial after adding memory records
-        let w_l_comm = Self::commit(&proving_key.polynomials.w_l, &proving_key.crs)?;
-        let w_r_comm = Self::commit(&proving_key.polynomials.w_r, &proving_key.crs)?;
-        let w_o_comm = Self::commit(&proving_key.polynomials.w_o, &proving_key.crs)?;
 
-        transcript.add_point(w_l_comm.into());
-        transcript.add_point(w_r_comm.into());
-        transcript.add_point(w_o_comm.into());
+        self.memory.witness_commitments.w_l_comm =
+            Self::commit(&proving_key.polynomials.w_l, &proving_key.crs)?;
+        self.memory.witness_commitments.w_r_comm =
+            Self::commit(&proving_key.polynomials.w_r, &proving_key.crs)?;
+        self.memory.witness_commitments.w_o_comm =
+            Self::commit(&proving_key.polynomials.w_o, &proving_key.crs)?;
+
+        transcript.add_point(self.memory.witness_commitments.w_l_comm.into());
+        transcript.add_point(self.memory.witness_commitments.w_r_comm.into());
+        transcript.add_point(self.memory.witness_commitments.w_o_comm.into());
 
         // Round is done since ultra_honk is no goblin flavor
         Ok(())
     }
 
+    // Compute sorted list accumulator and commitment
+    fn execute_sorted_list_accumulator_round(
+        &mut self,
+        transcript_inout: &mut Keccak256Transcript<P>,
+        proving_key: &ProvingKey<P>,
+    ) {
+        let mut transcript = Keccak256Transcript::<P>::default();
+        std::mem::swap(&mut transcript, transcript_inout);
+
+        self.memory.challenges.eta_1 = transcript.get_challenge();
+
+        let mut transcript = Keccak256Transcript::<P>::default();
+        transcript.add_scalar(self.memory.challenges.eta_1);
+        self.memory.challenges.eta_2 = transcript.get_challenge();
+
+        let mut transcript = Keccak256Transcript::<P>::default();
+        transcript.add_scalar(self.memory.challenges.eta_2);
+        self.memory.challenges.eta_3 = transcript.get_challenge();
+
+        transcript_inout.add_scalar(self.memory.challenges.eta_3);
+
+        // The memory record values are computed at the indicated indices as
+        // w4 = w3 * eta^3 + w2 * eta^2 + w1 * eta + read_write_flag;
+
+        // Compute read record values
+        for gate_idx in proving_key.memory_read_records.iter() {
+            let gate_idx = *gate_idx as usize;
+            let target = &mut self.memory.w_4[gate_idx];
+            *target += proving_key.polynomials.w_l[gate_idx] * self.memory.challenges.eta_1
+                + proving_key.polynomials.w_r[gate_idx] * self.memory.challenges.eta_2
+                + proving_key.polynomials.w_o[gate_idx] * self.memory.challenges.eta_3;
+        }
+
+        // Compute write record values
+        for gate_idx in proving_key.memory_write_records.iter() {
+            let gate_idx = *gate_idx as usize;
+            let target = &mut self.memory.w_4[gate_idx];
+            *target += proving_key.polynomials.w_l[gate_idx] * self.memory.challenges.eta_1
+                + proving_key.polynomials.w_r[gate_idx] * self.memory.challenges.eta_2
+                + proving_key.polynomials.w_o[gate_idx] * self.memory.challenges.eta_3
+                + P::ScalarField::one();
+        }
+
+        // Commit to lookup argument polynomials and the finalized (i.e. with memory records) fourth wire polynomial
+
+        todo!()
+    }
+
     pub fn prove(
-        self,
+        mut self,
         proving_key: ProvingKey<P>,
         public_inputs: Vec<P::ScalarField>,
     ) -> HonkProofResult<()> {
         let mut transcript = Keccak256Transcript::default();
 
+        // Add circuit size public input size and public inputs to transcript
         Self::execute_preamble_round(&mut transcript, &proving_key, &public_inputs)?;
-        Self::execute_wire_commitments_round(&mut transcript, &proving_key)?;
+        // Compute first three wire commitments
+        self.execute_wire_commitments_round(&mut transcript, &proving_key)?;
+        // Compute sorted list accumulator and commitment
+        self.execute_sorted_list_accumulator_round(&mut transcript, &proving_key);
+
+        debug_assert_eq!(
+            proving_key.polynomials.w_l.len(),
+            proving_key.polynomials.w_r.len()
+        );
+        debug_assert_eq!(
+            proving_key.polynomials.w_l.len(),
+            proving_key.polynomials.w_o.len()
+        );
+        self.memory
+            .w_4
+            .resize(proving_key.polynomials.w_l.len(), P::ScalarField::zero());
 
         Ok(())
     }
--- co-noir/ultrahonk/src/types.rs
+++ co-noir/ultrahonk/src/types.rs
@@ -10,10 +10,61 @@ pub struct ProvingKey<P: Pairing> {
     pub num_public_inputs: u32,
     pub pub_inputs_offset: u32,
     pub polynomials: Polynomials<P>,
+    pub memory_read_records: Vec<u32>,
+    pub memory_write_records: Vec<u32>,
 }
 
 pub struct Polynomials<P: Pairing> {
     pub w_l: Vec<P::ScalarField>,
     pub w_r: Vec<P::ScalarField>,
     pub w_o: Vec<P::ScalarField>,
+}
+
+pub struct Challenges<P: Pairing> {
+    pub eta_1: P::ScalarField,
+    pub eta_2: P::ScalarField,
+    pub eta_3: P::ScalarField,
+}
+
+// pub struct WitnessEntities<P: Pairing> {}
+
+pub struct WitnessCommitments<P: Pairing> {
+    pub w_l_comm: P::G1,
+    pub w_r_comm: P::G1,
+    pub w_o_comm: P::G1,
+}
+pub struct ProverMemory<P: Pairing> {
+    pub w_4: Vec<P::ScalarField>,
+    pub witness_commitments: WitnessCommitments<P>,
+    pub challenges: Challenges<P>,
+}
+
+impl<P: Pairing> Default for WitnessCommitments<P> {
+    fn default() -> Self {
+        Self {
+            w_l_comm: Default::default(),
+            w_r_comm: Default::default(),
+            w_o_comm: Default::default(),
+        }
+    }
+}
+
+impl<P: Pairing> Default for Challenges<P> {
+    fn default() -> Self {
+        Self {
+            eta_1: Default::default(),
+            eta_2: Default::default(),
+            eta_3: Default::default(),
+        }
+    }
+}
+
+impl<P: Pairing> Default for ProverMemory<P> {
+    fn default() -> Self {
+        Self {
+            w_4: Default::default(),
+            witness_commitments: Default::default(),
+            challenges: Default::default(),
+        }
+    }
 }
