6f116953e14ee4f06a2fbf891d06b22583648c36
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-09-13 13:29:58 +0200

    start rewriting the transcript

--- co-noir/ultrahonk/src/transcript.rs
+++ co-noir/ultrahonk/src/transcript.rs
@@ -2,7 +2,7 @@ use ark_ec::{pairing::Pairing, AffineRepr};
 use ark_ff::PrimeField;
 use ark_serialize::CanonicalSerialize;
 use sha3::{Digest, Keccak256};
-use std::marker::PhantomData;
+use std::{collections::HashMap, marker::PhantomData, ops::Index};
 
 // TODO this whole file is copied from our co-Plonk and should probably be adapted
 
@@ -13,14 +13,16 @@ where
     D: Digest,
     P: Pairing,
 {
-    digest: D,
-    phantom_data: PhantomData<P>,
+    proof_data: Vec<P::ScalarField>,
+    // digest: D,
+    phantom_data: PhantomData<D>,
 }
 
 impl<P: Pairing> Default for Keccak256Transcript<P> {
     fn default() -> Self {
         Self {
-            digest: Default::default(),
+            proof_data: Default::default(),
+            // digest: Default::default(),
             phantom_data: Default::default(),
         }
     }
@@ -31,48 +33,111 @@ where
     D: Digest,
     P: Pairing,
 {
-    pub(super) fn add_scalar(&mut self, scalar: P::ScalarField) {
-        let mut buf = vec![];
-        scalar
-            .serialize_uncompressed(&mut buf)
-            .expect("Can Fr write into Vec<u8>");
-        buf.reverse();
-        self.digest.update(&buf);
+    fn send_to_verifier(&mut self, label: &str, elements: Vec<P::ScalarField>) {
+        self.proof_data.extend(elements);
     }
 
-    pub(super) fn add_point(&mut self, point: P::G1Affine) {
-        let byte_len: usize = P::BaseField::MODULUS_BIT_SIZE
-            .div_ceil(8)
-            .try_into()
-            .expect("u32 fits into usize");
-        let mut buf = Vec::with_capacity(byte_len);
-        if let Some((x, y)) = point.xy() {
-            x.serialize_uncompressed(&mut buf)
-                .expect("Can write Fq into Vec<u8>");
-            buf.reverse();
-            self.digest.update(&buf);
-            buf.clear();
-            y.serialize_uncompressed(&mut buf)
-                .expect("Can write Fq into Vec<u8>");
-            buf.reverse();
-            self.digest.update(&buf);
-        } else {
-            // we are at infinity - in this case, snarkjs writes (MODULUS_BIT_SIZE / 8) Zero-bytes
-            // to the input buffer. If we serialize with arkworks, we would
-            // get (MODULUS_BIT_SIZE / 8 - 1) Zero-bytes with a trailing byte indicating the length of
-            // the serialized group element, resulting in an incompatible hash. Therefore we simple resize
-            // the buffer with Zeros and write it to the hash instance.
-            buf.resize(byte_len * 2, 0);
-            self.digest.update(&buf);
+    // pub(super) fn add_scalar(&mut self, scalar: P::ScalarField) {
+    //     let mut buf = vec![];
+    //     scalar
+    //         .serialize_uncompressed(&mut buf)
+    //         .expect("Can Fr write into Vec<u8>");
+    //     buf.reverse();
+    //     self.digest.update(&buf);
+    // }
+
+    // pub(super) fn add_point(&mut self, point: P::G1Affine) {
+    //     let byte_len: usize = P::BaseField::MODULUS_BIT_SIZE
+    //         .div_ceil(8)
+    //         .try_into()
+    //         .expect("u32 fits into usize");
+    //     let mut buf = Vec::with_capacity(byte_len);
+    //     if let Some((x, y)) = point.xy() {
+    //         x.serialize_uncompressed(&mut buf)
+    //             .expect("Can write Fq into Vec<u8>");
+    //         buf.reverse();
+    //         self.digest.update(&buf);
+    //         buf.clear();
+    //         y.serialize_uncompressed(&mut buf)
+    //             .expect("Can write Fq into Vec<u8>");
+    //         buf.reverse();
+    //         self.digest.update(&buf);
+    //     } else {
+    //         // we are at infinity - in this case, snarkjs writes (MODULUS_BIT_SIZE / 8) Zero-bytes
+    //         // to the input buffer. If we serialize with arkworks, we would
+    //         // get (MODULUS_BIT_SIZE / 8 - 1) Zero-bytes with a trailing byte indicating the length of
+    //         // the serialized group element, resulting in an incompatible hash. Therefore we simple resize
+    //         // the buffer with Zeros and write it to the hash instance.
+    //         buf.resize(byte_len * 2, 0);
+    //         self.digest.update(&buf);
+    //     }
+    // }
+
+    // pub(super) fn add(&mut self, data: impl AsRef<[u8]>) {
+    //     self.digest.update(data);
+    // }
+
+    // pub(super) fn get_challenge(self) -> P::ScalarField {
+    //     let bytes = self.digest.finalize();
+    //     P::ScalarField::from_be_bytes_mod_order(&bytes)
+    // }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Default)]
+struct RoundData {
+    challenge_label: Vec<String>,
+    entries: Vec<(String, usize)>,
+}
+
+impl RoundData {
+    pub(crate) fn print(&self) {
+        for label in self.challenge_label.iter() {
+            println!("\tchallenge: {}", label);
+        }
+        for entry in self.entries.iter() {
+            println!("\telement ({}): {}", entry.1, entry.0);
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+struct TranscriptManifest {
+    manifest: HashMap<usize, RoundData>,
+}
+
+impl TranscriptManifest {
+    pub(crate) fn print(&self) {
+        for round in self.manifest.iter() {
+            println!("Round: {}", round.0);
+            round.1.print();
         }
     }
 
-    pub(super) fn add(&mut self, data: impl AsRef<[u8]>) {
-        self.digest.update(data);
+    pub(crate) fn add_challenge(&mut self, round: usize, labels: Vec<String>) {
+        self.manifest
+            .entry(round)
+            .or_insert_with(RoundData::default)
+            .challenge_label
+            .extend(labels);
     }
 
-    pub(super) fn get_challenge(self) -> P::ScalarField {
-        let bytes = self.digest.finalize();
-        P::ScalarField::from_be_bytes_mod_order(&bytes)
+    pub(crate) fn add_entry(&mut self, round: usize, element_label: String, element_size: usize) {
+        self.manifest
+            .entry(round)
+            .or_insert_with(RoundData::default)
+            .entries
+            .push((element_label, element_size));
+    }
+
+    pub(crate) fn size(&self) -> usize {
+        self.manifest.len()
+    }
+}
+
+impl Index<usize> for TranscriptManifest {
+    type Output = RoundData;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.manifest[&index]
     }
 }
