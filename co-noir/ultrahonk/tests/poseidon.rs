2bd2e27eaed4a31a84ce40426d151559c14c8c46
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-09-18 13:39:54 +0200

    can now deserialize barretenberg

--- co-noir/ultrahonk/tests/poseidon.rs
+++ co-noir/ultrahonk/tests/poseidon.rs
@@ -1,5 +1,14 @@
+use acir::{native_types::WitnessStack, FieldElement};
+use noirc_artifacts::program::ProgramArtifact;
+
 #[test]
 fn poseidon_test() {
-    const BYTECODE_PATH: &str = "example/poseidon/poseidon.rs";
-    const WITNESS_PATH: &str = "example/poseidon/poseidon.json";
+    let program =
+        std::fs::read_to_string("../../test_vectors/noir/poseidon/kat/poseidon.json").unwrap();
+    let program_artifact = serde_json::from_str::<ProgramArtifact>(&program)
+        .expect("failed to parse program artifact");
+
+    let should_witness = std::fs::read("../../test_vectors/noir/poseidon/kat/poseidon.gz").unwrap();
+
+    let should_witness = WitnessStack::<FieldElement>::try_from(should_witness.as_slice()).unwrap();
 }
